033cfa28e74afb1a8dc7910254ea331734939ba8
Matts966 <Matts966@users.noreply.github.com>
2019-10-15 17:25:53 +0900

    Add a simple test

---
+++ tests/stdout.c
@@ -0,0 +1,7 @@
+#include <tk/tkernel.h>
+#include <tm/tmonitor.h>
+#include <libstr.h>
+
+EXPORT INT usermain( void ) {
+	tm_putstring("output!\n");
+}
---
+++ tests/stdout.ts
@@ -0,0 +1,3 @@
+import * as tkernel from "../tkernel"
+
+console.log("output!")
