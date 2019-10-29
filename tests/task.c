ca56529cf97dc3be8f3288d765ab7ab521fc6b85
Matts966 <Matts966@users.noreply.github.com>
2019-10-29 14:49:11 +0900

    Modify c source in test cases to make it easy to generate

--- tests/task.c
+++ tests/task.c
@@ -2,11 +2,11 @@
 #include <tm/tmonitor.h>
 #include <libstr.h>
 
-typedef enum { TSK_A, OBJ_KIND_NUM } OBJ_KIND;
+typedef enum { TASK_A, OBJ_KIND_NUM } OBJ_KIND;
 EXPORT ID ObjID[OBJ_KIND_NUM];
 
-EXPORT void tsk_a(INT stacd, VP exinf);
-EXPORT void tsk_a(INT stacd, VP exinf) {
+EXPORT void task_a(INT stacd, VP exinf);
+EXPORT void task_a(INT stacd, VP exinf) {
 	int i;
 	for ( i = 0; i < 3; i++ ) {
 		tm_putstring("output!");
@@ -18,16 +18,17 @@ EXPORT INT usermain( void ) {
 	T_CTSK t_ctsk;
 	ID objid;
 	t_ctsk.tskatr = TA_HLNG | TA_DSNAME;
+
 	t_ctsk.stksz = 1024;
 	t_ctsk.itskpri = 1;
-	STRCPY( (char *)t_ctsk.dsname, "tsk_a");
+	STRCPY( (char *)t_ctsk.dsname, "task_a");
+	t_ctsk.task = task_a;
 	if ( (objid = tk_cre_tsk( &t_ctsk )) <= E_OK ) {
-		tm_putstring(" *** Failed in the creation of tsk_a.\n");
+		tm_putstring(" *** Failed in the creation of task_a.\n");
 		return 1;
 	}
-	ObjID[TSK_A] = objid;
-	t_ctsk.task = tsk_a;
+	ObjID[TASK_A] = objid;
 	while(1) {
-		tk_sta_tsk( ObjID[TSK_A], 0 );
+		tk_sta_tsk( ObjID[TASK_A], 0 );
 	}
 }
