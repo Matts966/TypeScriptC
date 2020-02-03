1e67898d2e9c518e7b16a479add73f118b325b6e
Matts966 <Matts966@users.noreply.github.com>
2020-02-03 18:14:30 +0900

    Use typelevelLR for fluent generator API but generated code is too big now

---
+++ bridge/unary-expression/app.ts
@@ -0,0 +1,8 @@
+import * as UnaryExpression from './UnaryExpression'
+
+// UnaryExpression.begin().sizeof()
+
+class Node10 {
+	private _Node10Brand: boolean = true
+	constructor(public arg1 : UnaryExpression._UnaryPostfixExpression) {}
+}
